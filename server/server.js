afc9a4f6ef6251b6b457707a491fda933201fca9
aggg6 <aggg6@mail.umkc.edu>
2017-10-26 18:29:34 -0500

    server.js updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const express= require('express');
-var app = express();
+
+const port = process.env.PORT||3000;
 const publicPath = path.join(__dirname,'../public');
+var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-  console.log('server is up on port 3000');
+app.listen(port,()=>{
+  console.log(`Server is up on port ${port}`);
 })
