81897973ef1a82a19568fe614f37641125744137
aggg6 <aggg6@mail.umkc.edu>
2017-10-28 15:47:13 -0500

    geo location added

--- public/js/index.js
+++ public/js/index.js
@@ -16,14 +16,36 @@ socket.on('newMessage',function(message){
   jQuery('#messages').append(li);
 })
 
+socket.on('newLocationMessage',function(message){
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current Location</a>');
+  li.text(`${message.from}: `)
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+})
+
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
   socket.emit('createMessage',{
     from : 'User',
     text : jQuery('[name=message]').val()
   },function(data){
-
     console.log('got it',data);
-
   })
 })
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+   return alert('geolocation is not supported by your browser');
+  }
+  navigator.geolocation.getCurrentPosition(function(positions){
+    socket.emit('createLocationMessage',{
+      longitude:positions.coords.longitude,
+      latitude : positions.coords.latitude
+    });
+  },function(){
+    alert('unable to fetch your location');
+  });
+});
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,27 @@
 var expect = require('expect');
 
-var generateMessage = require('./message');
+var {generateMessage,generateLocationMessage} = require('./message');
 
 describe('generateMessage',()=>{
   it('should return the correct values',()=>{
     var from = 'satish';
     var text = 'hey what\'s going on';
-    var genMsg = generateMessage.generateMessage(from,text);
+    var genMsg = generateMessage(from,text);
     expect(genMsg).toInclude({from,text});
     expect(genMsg.createdAt).toBeA('number');
+  })
+})
+
+describe('generateLocationMessage',()=>{
+  it('should return the coorect link for location',()=>{
+    var from = 'naveen';
+    var location = {
+      longitude : 12345,
+      latitude : 12345
+    }
+    var locurl = generateLocationMessage(from,location.latitude,location.longitude);
+    expect(locurl.from).toBe(from);
+    expect(locurl.createdAt).toBeA('number');
+    expect(locurl.url).toBe(`https://www.google.com/maps?q=12345,12345`);
   })
 })
