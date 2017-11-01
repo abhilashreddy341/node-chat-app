2c328210c59405ddd2b1c7bb59333328f5062382
aggg6 <aggg6@mail.umkc.edu>
2017-11-01 16:37:05 -0500

    Timestamp formatted

--- public/js/index.js
+++ public/js/index.js
@@ -10,16 +10,18 @@ socket.on('disconnect',function(){
   console.log('Disconnected from the server');
 })
 socket.on('newMessage',function(message){
+  var formattedtime = moment(message.createdAt).format('h:mm a');
   console.log('New message arrived',message);
   var li= jQuery('<li></li>');
-  li.text(`${message.from}:${message.text}`);
+  li.text(`${message.from} ${formattedtime} :${message.text}`);
   jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage',function(message){
+  var formattedtime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current Location</a>');
-  li.text(`${message.from}: `)
+  li.text(`${message.from} ${formattedtime} : `);
   a.attr('href',message.url);
   li.append(a);
   jQuery('#messages').append(li);
