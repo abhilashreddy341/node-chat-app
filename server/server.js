8037dfd2475d0dedff16df64fbdcbfc2e10b7bf2
aggg6 <aggg6@mail.umkc.edu>
2017-11-02 19:40:31 -0500

    sending messages only to room

--- server/server.js
+++ server/server.js
@@ -35,12 +35,17 @@ io.on('connection',(socket)=>{
   })
 
   socket.on('createMessage',(message,callback)=>{
-    console.log('createMessage',message);
-    io.emit('newMessage',generateMessage(message.from,message.text));
+    var user = users.getUser(socket.id);
+    if(user&&isStringValid(message.text)){
+        io.to(user[0].room).emit('newMessage',generateMessage(user[0].name,message.text));
+    }
      callback();
   });
   socket.on('createLocationMessage',(coords)=>{
-    io.emit('newLocationMessage',generateLocationMessage('admin',coords.latitude,coords.longitude));
+    var user = users.getUser(socket.id);
+    if(user){
+    io.to(user[0].room).emit('newLocationMessage',generateLocationMessage(user[0].name,coords.latitude,coords.longitude));
+  }
   })
   socket.on('disconnect',()=>{
     var user = users.removeUser(socket.id);
